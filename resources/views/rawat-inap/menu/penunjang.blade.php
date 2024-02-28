b120d2e1cf71efbf488953258775425958bdedff
Fikri Ramadhan <ramadhan@kabayan.id>
2024-02-28 07:49:51 +0700

    penunjang

--- resources/views/rawat-inap/menu/penunjang.blade.php
+++ resources/views/rawat-inap/menu/penunjang.blade.php
@@ -15,21 +15,23 @@
                 <td>{{ $p->status_pemeriksaan }}</td>
                 <td>
                     <ul>
-                        @foreach (json_decode($p->pemeriksaan_penunjang) as $pen)
-                            @if ($p->jenis_penunjang == 'Lab')
-                                @foreach ($lab as $l)
-                                    @if ($l->id == $pen->tindakan_lab)
-                                        <li>{{ $l->nama_pemeriksaan }}</li>
-                                    @endif
-                                @endforeach
-                            @else
-                                @foreach ($radiologi as $rad)
-                                    @if ($rad->id == $pen->tindakan_rad)
-                                        <li>{{ $rad->nama_tindakan }}</li>
-                                    @endif
-                                @endforeach
-                            @endif
-                        @endforeach
+                        @if ($p->pemeriksaan_penunjang != 'null')
+                            @foreach (json_decode($p->pemeriksaan_penunjang) as $pen)
+                                @if ($p->jenis_penunjang == 'Lab')
+                                    @foreach ($lab as $l)
+                                        @if ($l->id == $pen->tindakan_lab)
+                                            <li>{{ $l->nama_pemeriksaan }}</li>
+                                        @endif
+                                    @endforeach
+                                @else
+                                    @foreach ($radiologi as $rad)
+                                        @if ($rad->id == $pen->tindakan_rad)
+                                            <li>{{ $rad->nama_tindakan }}</li>
+                                        @endif
+                                    @endforeach
+                                @endif
+                            @endforeach
+                        @endif
                     </ul>
                 </td>
             </tr>
