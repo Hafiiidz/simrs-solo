0074b5ed3e7742532561f7f5602eae7f3db9b7c3
Fikri Ramadhan <ramadhan@kabayan.id>
2024-03-17 08:48:29 +0700

    commit

--- app/Helpers/SatusehatAuthHelper.php
+++ app/Helpers/SatusehatAuthHelper.php
@@ -22,11 +22,12 @@ class SatusehatAuthHelper
     }
     public static function generate_token(){
         $url = env('PROD_AUTH_URL_SS');
-        // return $url;
+        return $url;
         $response = Http::asForm()->withOptions(["verify" => SatusehatAuthHelper::ssl()])->post($url.'/accesstoken?grant_type=client_credentials', [
             'client_id' =>env('PROD_CLIENT_ID_SS'),
             'client_secret' =>env('PROD_CLIENT_SECRET_SS')
         ]);
+
         return $response->json();
     }
 }
